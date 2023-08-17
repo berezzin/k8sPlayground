8dca2dd9d0860274c3147416dfdbfa10cdc19871
berezzin <biarezzin@gmail.com>
2023-08-17 23:55:14 +0200

    Added secret

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
 import socket
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -8,4 +12,5 @@ app = FastAPI()
 @app.get('/')
 def index(request: Request):
     client_ip = request.client.host
-    return f'Server ip is {socket.gethostbyname(socket.gethostname())} and client ip is {client_ip}'
+    return (f'Server ip is {socket.gethostbyname(socket.gethostname())} and client ip is {client_ip}. '
+            f'Also user from environment: {os.getenv("MY_ENV")}')
